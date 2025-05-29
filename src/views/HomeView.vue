30a93e6545fb5866c46396059a8cc9f77f4eba3a
nate <nate.benson@mealmagic.com>
2025-05-29 13:47:34 -0400

    Added Refresh button to mobile scaling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -33,20 +33,34 @@
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <!-- City navbar -->
           <div>
-            <div class="grid grid-cols-1 sm:hidden">
-              <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-              <select
-                aria-label="Select a tab"
-                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-2 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            <div class="flex flex-row">
+              <div class="flex-1 grid grid-cols-1 sm:hidden pr-4">
+                <select
+                  aria-label="Select a tab"
+                  class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-2 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  @change="citySelected($event.target.selectedIndex)"
+                >
+                  <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">
+                    {{ tab.name }}
+                  </option>
+                </select>
+                <ChevronDownIcon
+                  class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end fill-gray-500"
+                  aria-hidden="true"
+                />
+              </div>
+              <button
+                class="bg-gray-700 text-gray-300 hover:bg-gray-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium hover:cursor-pointer"
+                @click="
+                  fetchWeather(
+                    tabs.find((tab) => tab.current)!.lat,
+                    tabs.find((tab) => tab.current)!.lon
+                  )
+                "
               >
-                <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">
-                  {{ tab.name }}
-                </option>
-              </select>
-              <ChevronDownIcon
-                class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end fill-gray-500"
-                aria-hidden="true"
-              />
+                <ArrowPathIcon class="inline-block h-5 w-5 mr-1 -mt-1" aria-hidden="true" />
+                Refresh
+              </button>
             </div>
             <div class="hidden sm:flex sm:items-center sm:justify-between">
               <nav class="flex space-x-4" aria-label="Tabs">
@@ -67,7 +81,7 @@
                 </button>
               </nav>
               <button
-                class="bg-gray-700 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium hover:cursor-pointer"
+                class="bg-gray-700 text-gray-300 hover:bg-gray-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium hover:cursor-pointer"
                 @click="
                   fetchWeather(
                     tabs.find((tab) => tab.current)!.lat,
